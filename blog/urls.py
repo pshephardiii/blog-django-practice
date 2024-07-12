8c727188b082bff89da0fdf61bb69ecd7c3d5451
Paul Shephard <paulshephard598@gmail.com>
2024-07-12 17:04:51 -0400

    blog urls and views basic setup

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
-urlspattern = [
-    path()
+urlpatterns = [
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts_page"),
+    path("posts/<slug:slug>", views.post_detail, name="post_detail-page") 
+    # slug refers to the concept of a unique identifier in the url that's dynamic
 ]
